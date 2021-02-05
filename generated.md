b66d04242a2aa972e520c093ae46c22c2e7689d1
MCannon33 <marialcannon@gmail.com>
2021-02-05 00:57:01 -0500

    add table of contents and link github

--- generated.md
+++ generated.md
@@ -1,16 +1,33 @@
-# Riverdale
+# jhgjghjghj
+##Table of Contents
+* 1.[Description](#description)
+* 2.[Instructions](#instructions)
+* 3.[Usage](#usage)
+* 4.[Contribution](#contribution)
+* 5.[Tests](#tests)
+* 6.[License](#license)
+* 7.[Questions](#questions)
+
+
 ## Description 
-Teen show
+kkkk
+___
 ## Instructions 
-mystery style
+kkkk
+___
 ## Usage
-run
+kkkk
+___
 ## Contribution
-spy
+kkkk
+___
 ## Tests
-spy again
+kkkk
+___
 ## License
 Apache
+___
 ## Questions
-NancyD
-NancyD@yahoo.com
+For more information please contact me at: 
+[GitHub](kkkkkk)
+kkkkkkk
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,22 +1,43 @@
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+##Table of Contents
+* 1.[Description](#description)
+* 2.[Instructions](#instructions)
+* 3.[Usage](#usage)
+* 4.[Contribution](#contribution)
+* 5.[Tests](#tests)
+* 6.[License](#license)
+* 7.[Questions](#questions)
+
+
 ## Description 
 ${data.description}
+___
 ## Instructions 
 ${data.instructions}
+___
 ## Usage
 ${data.usage}
+___
 ## Contribution
 ${data.contribution}
+___
 ## Tests
 ${data.tests}
+___
 ## License
 ${data.license}
+___
 ## Questions
-${data.github}
+For more information please contact me at: 
+[GitHub](${data.github})
 ${data.email}
 `;
 }
 
 module.exports = generateMarkdown;
+
+// [![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+// [![Made withJupyter](https://img.shields.io/badge/Made%20with-Jupyter-orange?style=for-the-badge&logo=Jupyter)](https://jupyter.org/try)
+// [![Open In Collab](https://colab.research.google.com/assets/colab-badge.svg)
